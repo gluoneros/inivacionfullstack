6c045ea69fd3236ad0665bea4a1b43bcc7e2eab6
gluoneros <gluoneros@gmail.com>
2025-07-18 19:00:48 -0500

    Agregar modelo CustomUser con campos de usuario, correo y rol

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    ]
+    
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    
+    def __str__(self):
+        return self.username
